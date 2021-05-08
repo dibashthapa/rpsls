083c4ba3c05f033707eec442c563fa2ed75db98c
= <=>
2021-05-08 09:42:25 +0545

    new app

---
+++ src/components/Play.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <img :src="getSource" :alt="msg" @click="clicked" />
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      // source: "",
+    };
+  },
+  methods: {
+    clicked() {
+      this.$emit("thichyo", this.msg);
+    },
+  },
+  computed: {
+    getSource() {
+      return require(`@/assets/rpsls/${this.msg}.png`);
+    },
+  },
+};
+</script>
---
+++ src/views/Game.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>hello</h1>
+    <Play v-for="s in stuff" :key="s.index" :msg="s" @thichyo="doThis"></Play>
+  </div>
+</template>
+<script>
+import Play from "@/components/Play";
+
+export default {
+  data() {
+    return {
+      stuff: ["Rock", "Paper", "Scissor", "Lizard", "Spock"],
+    };
+  },
+  methods: {
+    doThis(msg) {
+      console.log("User Clicked " + msg);
+    },
+  },
+  components: { Play },
+};
+</script>
