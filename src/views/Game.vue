ed63e3a60fb75f6380bef6119167f58865957725
= <=>
2021-05-09 12:00:42 +0545

    added switch case login

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -15,7 +15,39 @@ export default {
   },
   methods: {
     doThis(msg) {
-      console.log("User Clicked " + msg);
+      let randomInt = Math.floor(Math.random() * 3);
+      let computerSelected = this.stuff[randomInt];
+      console.log(this.checkWinner(msg, computerSelected));
+    },
+    checkWinner(user, computer) {
+      if (user === computer) {
+        return "Tied";
+      }
+      switch (user) {
+        case "Rock":
+          switch (computer) {
+            case "Paper":
+              return `Computer chose ${computer}, User chose ${user}. Computer Won`;
+            case "Scissor":
+              return `Computer chose ${computer}, User chose ${user}. User Won`;
+          }
+
+        case "Paper":
+          switch (computer) {
+            case "Rock":
+              return `Computer chose ${computer}, User chose ${user}. Computer Won`;
+            case "Scissor":
+              return `Computer chose ${computer}, User chose ${user}. User Won`;
+          }
+
+        case "Scissor":
+          switch (computer) {
+            case "Rock":
+              return `Computer chose ${computer}, User chose ${user}. Computer Won`;
+            case "Paper":
+              return `Computer chose ${computer}, User chose ${user}. User Won`;
+          }
+      }
     },
   },
   components: { Play },
