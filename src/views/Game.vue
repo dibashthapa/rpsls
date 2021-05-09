cc22c920259f40d5f7ba10534a5163bebcb02849
= <=>
2021-05-09 12:28:47 +0545

    Reactive value

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,7 +1,13 @@
 <template>
   <div>
-    <h1>hello</h1>
-    <Play v-for="s in stuff" :key="s.index" :msg="s" @thichyo="doThis"></Play>
+    <h1>{{ message }}</h1>
+    <Play
+      v-for="s in stuff"
+      :class="s"
+      :key="s.index"
+      :msg="s"
+      @thichyo="doThis"
+    ></Play>
   </div>
 </template>
 <script>
@@ -11,41 +17,74 @@ export default {
   data() {
     return {
       stuff: ["Rock", "Paper", "Scissor", "Lizard", "Spock"],
+      message: "Start The Game By Clicking Any Icon",
     };
   },
   methods: {
     doThis(msg) {
-      let randomInt = Math.floor(Math.random() * 3);
+      let randomInt = Math.floor(Math.random() * 5);
       let computerSelected = this.stuff[randomInt];
-      console.log(this.checkWinner(msg, computerSelected));
+      this.message = this.checkWinner(msg, computerSelected);
     },
     checkWinner(user, computer) {
+      console.log(
+        "\n User selected: ",
+        user,
+        "\n Computer selected: ",
+        computer
+      );
       if (user === computer) {
         return "Tied";
       }
       switch (user) {
         case "Rock":
           switch (computer) {
-            case "Paper":
-              return `Computer chose ${computer}, User chose ${user}. Computer Won`;
             case "Scissor":
-              return `Computer chose ${computer}, User chose ${user}. User Won`;
+            case "Lizard":
+              return "User Won";
+            case "Spock":
+            case "Paper":
+              return "Computer Won";
           }
 
         case "Paper":
           switch (computer) {
             case "Rock":
-              return `Computer chose ${computer}, User chose ${user}. Computer Won`;
+            case "Spock":
+              return "User Won";
             case "Scissor":
-              return `Computer chose ${computer}, User chose ${user}. User Won`;
+            case "Lizard":
+              return "Computer Won";
           }
 
         case "Scissor":
           switch (computer) {
-            case "Rock":
-              return `Computer chose ${computer}, User chose ${user}. Computer Won`;
             case "Paper":
-              return `Computer chose ${computer}, User chose ${user}. User Won`;
+            case "Lizard":
+              return "User Won";
+            case "Spock":
+            case "Rock":
+              return "Computer Won";
+          }
+
+        case "Lizard":
+          switch (computer) {
+            case "Paper":
+            case "Spock":
+              return "User Won";
+            case "Scissor":
+            case "Rock":
+              return "Computer Won";
+          }
+
+        case "Spock":
+          switch (computer) {
+            case "Rock":
+            case "Paper":
+              return "User Won";
+            case "Lizard":
+            case "Scissor":
+              return "Computer Won";
           }
       }
     },
@@ -53,3 +92,5 @@ export default {
   components: { Play },
 };
 </script>
+
+<style></style>
